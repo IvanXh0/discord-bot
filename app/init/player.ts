78ddb904338bf1993cfd7edba6aa0d0f147c9ad7
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-26 23:26:22 +0200

    last try for today

--- app/init/player.ts
+++ app/init/player.ts
@@ -2,12 +2,6 @@ import { Player } from "discord-player";
 import { client } from "./discordClient";
 import { YoutubeiExtractor } from "discord-player-youtubei";
 
-export const player = new Player(client, {
-  ytdlOptions: {
-    filter: "audioonly",
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
-  },
-});
+export const player = new Player(client);
 
 player.extractors.register(YoutubeiExtractor, {});
