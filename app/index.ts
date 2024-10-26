5b85270126dab2c52f8dfa69c0c34b7f4b23cb50
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-26 23:48:54 +0200

    last

--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,7 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
 import "dotenv/config";
 
 import "./init/discordClient";
@@ -15,6 +19,16 @@ import "./init/registerCommands";
 import { client } from "./init/discordClient";
 import { token } from "./config";
 import { connectDB } from "./init/database";
+
+app.use(express.json());
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Health check endpoint listening on port ${port}`);
+});
 
 connectDB();
 client.login(token);
