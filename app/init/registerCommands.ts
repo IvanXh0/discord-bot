fcc3024f1a83036b74fa6aeb1bb18b06b5662db2
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-26 21:26:11 +0200

    chore: add env distinction

--- app/init/registerCommands.ts
+++ app/init/registerCommands.ts
@@ -1,21 +1,38 @@
+// src/scripts/registerCommands.ts
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
 import { commands } from "../commands";
-import { clientId, guildId, token } from "../config";
+import { clientId, token, guildId } from "../config";
 
 const rest = new REST({ version: "9" }).setToken(token);
 
 const commandData = commands.map((command) => command.data.toJSON());
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 (async () => {
   try {
-    console.log("Started refreshing application (/) commands.");
+    if (isDevelopment && guildId) {
+      console.log(
+        "Started refreshing application (/) commands for development guild.",
+      );
 
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commandData,
-    });
+      await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commandData,
+      });
 
-    console.log("Successfully reloaded application (/) commands.");
+      console.log(
+        "Successfully reloaded application (/) commands for development guild.",
+      );
+    } else {
+      console.log("Started refreshing application (/) commands globally.");
+
+      await rest.put(Routes.applicationCommands(clientId), {
+        body: commandData,
+      });
+
+      console.log("Successfully reloaded application (/) commands globally.");
+    }
   } catch (error) {
     console.error(error);
   }
