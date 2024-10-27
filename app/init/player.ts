faec7e01ff748511aa03b2d732eb7d375b3c1e1e
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-27 23:15:36 +0100

    trying without an extractor

--- app/init/player.ts
+++ app/init/player.ts
@@ -1,7 +1,7 @@
 import { Player } from "discord-player";
 import { client } from "./discordClient";
-import { YoutubeiExtractor } from "discord-player-youtubei";
+// import { YoutubeiExtractor } from "discord-player-youtubei";
 
 export const player = new Player(client);
 
-player.extractors.register(YoutubeiExtractor, {});
+player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
