03ed8ef41d74aa8b65f873ceafb0b3a715539ecb
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-27 02:25:48 +0100

    add yt token

--- app/init/player.ts
+++ app/init/player.ts
@@ -1,23 +1,11 @@
 import { Player } from "discord-player";
 import { client } from "./discordClient";
 import { YoutubeiExtractor } from "discord-player-youtubei";
-import play from "play-dl";
 
-play.setToken({
-  useragent: [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-  ],
+export const player = new Player(client);
+
+player.extractors.register(YoutubeiExtractor, {
+  authentication: process.env.YT_ACCESS_TOKEN,
 });
 
-export const player = new Player(client, {
-  ytdlOptions: {
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
-    dlChunkSize: 0,
-  },
-  skipFFmpeg: false,
-});
-
-player.extractors.loadDefault();
-
-player.extractors.register(YoutubeiExtractor, {});
+player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
