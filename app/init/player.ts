ccb3f961eec3b4b9da2780dd7145df24c5759741
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-27 23:29:34 +0100

    ytdl

--- app/init/player.ts
+++ app/init/player.ts
@@ -2,6 +2,13 @@ import { Player } from "discord-player";
 import { client } from "./discordClient";
 // import { YoutubeiExtractor } from "discord-player-youtubei";
 
-export const player = new Player(client);
-
-player.extractors.loadDefault((ext) => ext !== "YouTubeExtractor");
+export const player = new Player(client, {
+  skipFFmpeg: false,
+  ytdlOptions: {
+    requestOptions: {
+      headers: {
+        cookie: process.env.YT_ACCESS_TOKEN,
+      },
+    },
+  },
+});
