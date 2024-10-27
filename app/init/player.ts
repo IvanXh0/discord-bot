84546b651b7e6955de63af76cec238d5f6bab59a
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2024-10-27 23:49:52 +0100

    idk what to do anymore

--- app/init/player.ts
+++ app/init/player.ts
@@ -1,16 +1,17 @@
 import { Player } from "discord-player";
 import { client } from "./discordClient";
-// import { YoutubeiExtractor } from "discord-player-youtubei";
+import { YoutubeiExtractor } from "discord-player-youtubei";
 
 export const player = new Player(client, {
+  useLegacyFFmpeg: false,
   skipFFmpeg: false,
-  ytdlOptions: {
-    requestOptions: {
-      headers: {
-        cookie: process.env.YT_ACCESS_TOKEN,
-      },
-    },
+});
+
+player.extractors.register(YoutubeiExtractor, {
+  streamOptions: {
+    useClient: "IOS",
+    highWaterMark: 2 * 1_024 * 1_024,
   },
 });
 
-player.extractors.loadDefault();
+player.extractors.loadDefault((ext) => !["YouTubeExtractor"].includes(ext));
